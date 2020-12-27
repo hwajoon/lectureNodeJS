2584fab30903b4620440e00675ef15ea153e3aa7
hwajoon <hwajunlee@daum.net>
2020-12-27 18:35:06 +0900

    add process argv example

---
+++ 04.consoleArgs/main.js
@@ -0,0 +1,19 @@
+var args = process.argv;
+console.log(args);
+console.log(args[0]);
+console.log(args[1]);
+console.log(args[2]);
+
+/*
+
+D:\workspace\lecture\NodeJS\04.consoleArgs>node main "abc"
+[
+  'C:\\Program Files\\nodejs\\node.exe',
+  'D:\\workspace\\lecture\\NodeJS\\04.consoleArgs\\main',
+  'abc'
+]
+C:\Program Files\nodejs\node.exe
+D:\workspace\lecture\NodeJS\04.consoleArgs\main
+abc
+
+*/
